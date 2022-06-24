ebd154c331968da17c47fc7b4495836c18b9ad00
Marcos <ma_cypriano@hotmail.com>
2022-06-24 11:17:26 -0300

    Add integration front & backend with basic products query

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,12 +1,26 @@
-import { getAccessToken, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { useGetProductsQuery } from "../../graphql/generated/graphql"
+import { getServerPageGetProducts, ssrGetProducts } from "../../graphql/generated/page"
+import { withApollo } from "../../lib/withApollo"
 
-export default function Home() {
+function Home({ data }) {
     const { user } = useUser()
+
+    // Query pelo client side
+    //const { data, loading, error } = useGetProductsQuery()
 
     return (
         <div>
             <h1>Hello World</h1>
+            <br />
 
+            <h2>Courses:</h2>
+            <pre>
+                {JSON.stringify(data.products, null, 2)}
+            </pre>
+            <br />
+
+            <h2>User's info:</h2>
             <pre>
                 {JSON.stringify(user, null, 2)}
             </pre>
@@ -14,12 +28,12 @@ export default function Home() {
     )
 }
 
+// Query pelo server side
 export const getServerSideProps = withPageAuthRequired({
-    getServerSideProps: async ({req, res}) => {
-        console.log(getAccessToken(req, res))
-
-        return {
-            props: {}
-        }
+    getServerSideProps: async (ctx) => {
+        return getServerPageGetProducts({}, ctx)
     }
 })
+export default withApollo(
+    ssrGetProducts.withPage()(Home)
+)
